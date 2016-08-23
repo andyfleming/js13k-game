49c5e4f92e20e520b60112b1db11417ba0c76f67
Andy Fleming <andy@andyfleming.com>
2016-08-22 22:33:41 -0700

    Adding file size output (for zip)

--- tasks/zip.js
+++ tasks/zip.js
@@ -1,11 +1,13 @@
 const gulp      = require('gulp');
 const zip       = require('gulp-zip');
 const rename    = require('gulp-rename');
+const size      = require('gulp-size');
 
 module.exports = () => {
   gulp.task( 'zip', [ 'build', 'template' ], () => {
     return gulp.src('./build/dist/*')
       .pipe( zip('game.zip') )
+      .pipe( size() )
       .pipe( gulp.dest('build') );
   });
 };
