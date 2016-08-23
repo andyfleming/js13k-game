4e5f237e0737d9f1813646b468dfdf0ec4060116
Andy Fleming <andy@andyfleming.com>
2016-08-23 00:44:16 -0700

    Adding config object

---
+++ src/js/config/config.js
@@ -0,0 +1,23 @@
+export default {
+
+  KEY: {
+    //MOVE_LEFT: 'ArrowLeft',
+    //MOVE_RIGHT: 'ArrowRight',
+    //JUMP: 'ArrowUp'
+    MOVE_LEFT: 'a',
+    MOVE_RIGHT: 'd',
+    JUMP: 'w'
+  },
+
+  MOVEMENT: {
+    JUMP_SPEED: 8,
+    JUMPS_ALLOWED: 2,
+    WALK_SPEED: 1,
+    WALK_SPEED_MAX: 4
+  },
+
+  WORLD: {
+    GRAVITY: 0.5
+  }
+
+}
