7bf0b2de45a0bc7b5193c884c8ef57918bf8c472
Andy Fleming <andy@andyfleming.com>
2016-08-23 00:44:11 -0700

    Adding keyboard controller

---
+++ src/js/controls/keyboard.js
@@ -0,0 +1,18 @@
+/**
+ * Returns an objects that will be updated when keys are pressed and released.
+ */
+export default function keyboard() {
+
+  var keys = {};
+
+  document.addEventListener('keydown', function(e) {
+    keys[e.key] = true;
+  });
+
+  document.addEventListener('keyup', function(e) {
+    keys[e.key] = false;
+  });
+
+  return keys;
+
+}
