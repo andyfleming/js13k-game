1b18b5cb4239e66eaf00de15b3f3993c3e099832
Andy Fleming <andy@andyfleming.com>
2016-08-27 15:13:19 -0700

    Switching watch event to trigger default tasks

--- tasks/watch.js
+++ tasks/watch.js
@@ -6,7 +6,7 @@ module.exports = () => {
   gulp.task( 'watch', () => {
     return gulp.watch(
       [ 'src/js/**/*.js', 'src/css/**/*.css', 'src/index.hbs' ],
-      [ 'build', 'css', 'template', 'zip' ]
+      [ 'default' ]
     );
   });
 };
