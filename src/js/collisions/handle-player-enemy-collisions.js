2338a9138b7322579b18100059a3feb5950581a8
Andy Fleming <andy@andyfleming.com>
2016-09-02 04:38:16 -0700

    Adding collision handler for enemy-player interactions

---
+++ src/js/collisions/handle-player-enemy-collisions.js
@@ -0,0 +1,27 @@
+import colliding from './colliding'
+
+export default function handlePlayerEnemyCollisions(player, enemy) {
+
+  if (!colliding(player.sprite, enemy.sprite)) {
+    return
+  }
+
+  // If the player is "dashing" simulate the enemy flying up in the air
+  if (player.dashing) {
+
+    enemy.sprite.posY = 233
+
+    setTimeout(function() {
+      enemy.sprite.posY = 263
+    }, 1000)
+
+  } else {
+
+    // Otherwise, simulate the player being knocked back and losing health
+    player.sprite.posX += (player.lastXDirection === 'l') ? 40 : -40
+
+  }
+
+
+
+}
