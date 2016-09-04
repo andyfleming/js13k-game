172bef5e78d67cce5abe7bae5778c4166e1f44f7
Andy Fleming <andy@andyfleming.com>
2016-09-04 16:19:33 -0700

    Cleanup and adding new sounds

--- src/js/sound/fx-controller.js
+++ src/js/sound/fx-controller.js
@@ -17,8 +17,6 @@ function createFx(sound) {
     pools[sound.toString()].players.push(player)
   }
 
-  console.log(pools)
-
   return function() {
     var pool = pools[sound.toString()]
 
@@ -31,5 +29,7 @@ function createFx(sound) {
 export default function FxController() {
   this.playJumpSound = createFx([0,,0.1472,,0.1057,0.3987,,0.257,,,,,,0.2339,,,,,0.8447,,,,,0.5])
   this.playHitSound = createFx([3,,0.0192,,0.1607,0.3916,,-0.5578,,,,,,,,,,,1,,,,,0.5])
-  this.playDashSound = createFx([0,0.4,0.29,0.36,0.34,0.5768,,0.08,0.24,0.23,,0.1399,0.11,0.05,,0.54,,,1,,,0.0167,,0.5])
+  this.playDashSound = createFx([0,,0.2123,,0.2532,0.46,,0.1208,,,,,,0.1542,,,,,1,,,0.1776,,0.5])
+  this.playShoot = createFx([0,,0.1577,0.0959,0.2757,0.5002,0.0559,-0.5441,,,,,,0.5769,-0.5699,,,,1,,,0.0851,,0.5])
+  this.playEnemyHit = createFx([0,,0.0968,,0.2989,0.2028,,-0.4879,,,,,,0.0846,,,,,1,,,0.0167,,0.5])
 }
